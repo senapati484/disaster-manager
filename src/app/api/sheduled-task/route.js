e5e1b73df574a34b127c5ec7e54b2d75e43789b2
“sayan <sayansenapati2544@gmail.com>
2025-04-26 10:02:05 +0530

    removing sheduled-task

--- src/app/api/sheduled-task/route.js
+++ src/app/api/sheduled-task/route.js
@@ -1,14 +1,14 @@
-// app/api/scheduled-task/route.js
+// // app/api/scheduled-task/route.js
 
-export const maxDuration = 300; // 5 minutes
-export const dynamic = "force-dynamic";
+// export const maxDuration = 300; // 5 minutes
+// export const dynamic = "force-dynamic";
 
-export async function GET(request) {
-  // Your code to be executed periodically
-  console.log("Scheduled task running...");
-  // ... perform your task here ...
-  return new Response(
-    JSON.stringify({ message: "Task executed successfully" }),
-    { status: 200 }
-  );
-}
+// export async function GET(request) {
+//   // Your code to be executed periodically
+//   console.log("Scheduled task running...");
+//   // ... perform your task here ...
+//   return new Response(
+//     JSON.stringify({ message: "Task executed successfully" }),
+//     { status: 200 }
+//   );
+// }
